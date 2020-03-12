efc81e5835ca0ef8d13d10bc52c3348c563d92dc
Bruno Pastre <pastre68@gmail.com>
2020-03-12 17:32:38 -0300

    Implemented UI enambling of sounds

--- NC4/audio/AudioManager.swift
+++ NC4/audio/AudioManager.swift
@@ -7,23 +7,30 @@ class AudioManager{
     private var songs = [SongLibrary : Song]()
     private var introsWithLoops = [IntroWithLoopLibrary : Song]()
 
+    private var shouldPlay: Bool!
+    
     private init() {
         SoundEffectLibrary.allCases.forEach( { soundEffects[$0] = SoundEffect(fileName: $0.rawValue) } )
         SongLibrary.allCases.forEach( { songs[$0] = Song(fileName: $0.rawValue) } )
         IntroWithLoopLibrary.allCases.forEach( { introsWithLoops[$0] = IntroWithLoop(introFileName: $0.info.intro, loopFileName: $0.info.loop) } )
+   
+        self.shouldPlay = !StorageFacade.instance.isAudioDisabled()
     }
     
     func play(soundEffect: SoundEffectLibrary) {
+        guard self.shouldPlay else { return }
         soundEffects[soundEffect]?.play()
     }
     
     func play(song: SongLibrary) {
+        guard self.shouldPlay else { return }
         stopSongs()
         stopIntrosWithLoops()
         songs[song]?.play()
     }
     
     func play(introWithLoop: IntroWithLoopLibrary) {
+        guard self.shouldPlay else { return }
         stopSongs()
         stopIntrosWithLoops()
         introsWithLoops[introWithLoop]?.play()
@@ -41,13 +48,16 @@ class AudioManager{
     private func stopIntrosWithLoops() {
         introsWithLoops.forEach( { $0.value.stop() } )
     }
-    
-    
-    func enable() {
+
+    func update() {
+        self.shouldPlay = !StorageFacade.instance.isAudioDisabled()
         
-    }
-    
-    func disable() {
-        
+        if !self.shouldPlay {
+            self.stopCurrentSong()
+            self.stopSongs()
+            self.stopIntrosWithLoops()
+        } else {
+            self.play(song: .theme)
+        }
     }
 }
